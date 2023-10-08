a0889be6d0adb555ba6d138f87a8a70bba551da4
White12352 <86940955+White12352@users.noreply.github.com>
2023-10-08 18:24:46 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -4,5 +4,7 @@ buildScript/lib/core/init.sh
 cd ..
 rm -rf sing-box
 git clone -b building https://github.com/PuerNya/sing-box.git sing-box
-cd tbax
+cd tbax/libcore
+go mod tidy
+cd ..
 buildScript/lib/core/build.sh
