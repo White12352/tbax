4101966fb656fe0259a6b3226225689997967600
White12352 <86940955+White12352@users.noreply.github.com>
2023-10-08 19:50:04 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -36,15 +36,15 @@ git config --global user.name "Your Name"
 git remote add MatsuriDayo https://github.com/MatsuriDayo/sing-box
 git fetch MatsuriDayo 1.6.a2
 git clean -f nekoutils/callback.go
-#git cherry-pick -x -n 008b4c7
-#git cherry-pick -x -n 6ade9e8
+git cherry-pick -x -n 008b4c7
+git cherry-pick -x -n 6ade9e8
 git cherry-pick -x -n 22afd17
 git cherry-pick -x -n fb5c7f6
-#git cherry-pick -x -n 9b22789
-#git cherry-pick -x -n 66bd740
-#git cherry-pick -x -n 89dd568
-#git cherry-pick -x -n 7af6671
-#git cherry-pick -x -n 762f212
+git cherry-pick -x -n 9b22789
+git cherry-pick -x -n 66bd740
+git cherry-pick -x -n 89dd568
+git cherry-pick -x -n 7af6671
+git cherry-pick -x -n 762f212
 go mod tidy
 cd ../tbax/libcore
 go mod tidy
