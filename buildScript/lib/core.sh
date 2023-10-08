dd0a7102e580cbbd020ce5bf731787f30c06b8cb
White12352 <86940955+White12352@users.noreply.github.com>
2023-10-08 20:19:01 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -5,7 +5,7 @@ cd ..
 rm -rf sing-box
 #git clone -b building https://github.com/PuerNya/sing-box.git sing-box
 #git clone -b dev-next-yaott https://github.com/CHIZI-0618/sing-box.git sing-box
-git clone -b dev-next https://github.com/SagerNet/sing-box.git sing-box
+git clone -b main https://github.com/qjebbs/sing-box.git sing-box
 svn co https://github.com/MatsuriDayo/sing-box/branches/1.6.a2/nekoutils sing-box/nekoutils
 cd sing-box
 git config --global user.email "you@example.com"
