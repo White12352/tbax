2e42ca7de3aadfbcb096765b6bd210bb877984c1
White12352 <86940955+White12352@users.noreply.github.com>
2023-10-08 13:27:21 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 buildScript/lib/core/init.sh
-cd ../sing-box
+cd ..
+git clone -b dev https://github.com/SagerNet/sing-quic sing-quic
+cd sing-box/test
 go mod tidy
+cd ..
+go mod tiyd
 cd ../sing-box-extra
 go mod tidy
-cd ../libneko
-go mod tidy
+#cd ../libneko
+#go mod tidy
 cd ../tbax
 buildScript/lib/core/build.sh
