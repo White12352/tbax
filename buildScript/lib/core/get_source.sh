863d36b74acd4f2d4e2362b5975925d453b1fa9d
White12352 <86940955+White12352@users.noreply.github.com>
2023-10-08 18:17:02 +0800

    Update get_source.sh

--- buildScript/lib/core/get_source.sh
+++ buildScript/lib/core/get_source.sh
@@ -15,12 +15,7 @@ if [ ! -d "sing-box-extra" ]; then
   git clone --no-checkout https://github.com/MatsuriDayo/sing-box-extra.git
 fi
 pushd sing-box-extra
-ls -la
 git checkout "$COMMIT_SING_BOX_EXTRA"
-ls -la
-#awk '{gsub("git clone --no-checkout https://github.com/MatsuriDayo/sing-box.git", "git clone -b building https://github.com/PuerNya/sing-box.git")}1' sing-box-extra/libs/get_source.sh > temp && mv -f temp sing-box-extra/libs/get_source.sh
-awk '{gsub("git clone --no-checkout https://github.com/MatsuriDayo/sing-box.git", "git clone -b building https://github.com/PuerNya/sing-box.git")}1' libs/get_source.sh > temp && mv -f temp libs/get_source.sh
-chmod +x libs/get_source.sh
 
 ENV_SING_BOX_EXTRA=1
 source $SRC_ROOT/buildScript/lib/core/get_source_env.sh
